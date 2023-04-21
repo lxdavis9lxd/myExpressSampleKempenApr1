1e42cf74cda7d86a13c745604d4a458182f561d5
lxdavis9lxd <lxdavis9lxd@gmail.com>
2023-04-21 23:41:20 +0000

    commit

--- expressExample/public/javascripts/Postcall.js
+++ expressExample/public/javascripts/Postcall.js
@@ -1,8 +1,24 @@
-const https = require('https');
-//function makeHttpPostCall(varHttpRequest,varToken, varBody) {
-//  // const postData = JSON.stringify(varBody);
-//   const options = {
-//     method: 'POST',
-//     headers: {
-//       'Authorization': `Bearer ${varToken}`,
-//       'Content-Type': 'application/json'
+async function makeHttpPostCall(varHttpRequest, varBody, varResult) {
+  const fetch = require('node-fetch');
+  await fetch(varHttpRequest, {
+    
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + global.DB_token,
+     'Content-Type': 'application/json'    
+     },
+body: varBody
+  
+})
+.then(response => response.json())
+.then(data => {
+console.log("this is data", data);
+varResult = data;
+return varResult;
+})
+.catch(error => {
+console.error(error);
+return "error";
+});
+  
+}
--- expressExample/routes/register.js
+++ expressExample/routes/register.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
+//var crypto = require('@trust/webcrypto');
+
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+//javascript
+const crypto = require('crypto');
+
+const password = 'password123';
+var salt = crypto.randomBytes(16);
+const iterations = 1000;
+const keylen = 64;
+const digest = 'sha512';
+
+crypto.pbkdf2(password, salt, iterations, keylen, digest, (err, derivedKey) => {
+  if (err) throw err;
+  console.log('encrypted Key', derivedKey.toString('hex'));  // '3745e48...08d59ae'
+});
+
+//decrpt
+crypto.subtle.importKey(
+  'raw',
+  new TextEncoder().encode(password),
+  { name: 'PBKDF2' },
+  false,
+  ['deriveBits']
+).then(key => {
+  crypto.subtle.deriveBits(
+    {
+      name: 'PBKDF2',
+      salt,
+      iterations,
+      hash: digest
+    },
+    key,
+    keylen * 8
+  ).then(derivedKey => {
+    console.log("decrypted value" ,new Uint8Array(derivedKey).toString());
+  });
+});
+
+
+
+
   res.render('register', { title: 'Products R Us Register' });
+
+
 });
 
 module.exports = router;
